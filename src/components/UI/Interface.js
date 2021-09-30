c91a51f0ab906c900c81d0c515546d87b07ae1eb
sadfool1 <takw14@gmail.com>
2021-10-01 00:36:36 +0800

    hello wolrd

---
+++ src/components/UI/Interface.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HelloWorld = () => {
+  return (
+    <div className="div1">
+      <p>Hello world</p>
+      <button> I am a button</button>
+    </div>
+  );
+};
+
+export default HelloWorld;
