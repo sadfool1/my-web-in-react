e4a1f9b56aa197380fd3e1fe6cf887ccbead0ad7
sadfool1 <takw14@gmail.com>
2021-10-29 21:31:24 +0800

    playing with new styled components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import "./App.css";
 import HelloWorld from "./components/UI/Interface/Interface";
 
 function App() {
   return (
     <div className="App">
-      <HelloWorld> Hello World! </HelloWorld>
+      <HelloWorld></HelloWorld>
     </div>
   );
 }
--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,11 +1,41 @@
 import React from "react";
-import Button from "Button.css";
+import styled, { css } from "styled-components";
 
+const MyButton = styled.button`
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid palevioletred;
+  color: palevioletred;
+  margin: 0.5em 1em;
+  padding: 0.25em 1em;
+
+  ${props =>
+    props.primary &&
+    css`
+      background: palevioletred;
+      color: white;
+    `}
+`;
+
+const Container = styled.div`
+  text-align: center;
+`;
+
+const clickHandler = e => {
+  e.preventDefault();
+};
+
+function changeBackground(e) {
+  e.target.style.background = "red";
+}
 const Button = () => {
   return (
-    <div>
-      <button className="button"> Button </button>
-    </div>
+    <Container>
+      <MyButton onClick={clickHandler} onMouseOver={changeBackground}>
+        Normal Button
+      </MyButton>
+      <MyButton primary>Primary Button</MyButton>
+    </Container>
   );
 };
 
